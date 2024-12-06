aa5235bb7cb0d21572fbd0abe90a6b54b998e16f
majidsh97 <sharghi.majid@gmail.com>
2024-12-06 22:13:41 +0100

    .

--- project/config/config.py
+++ project/config/config.py
@@ -5,6 +5,6 @@ from  os.path import join, dirname
 root_path = '/home/cip/ce/ix05ogym/Majid/MADE/'
 
 __sp = join(root_path,'settings.toml')
-settings = Dynaconf(settings_files=[__sp] )
+settings = Dynaconf(settings_files=['./settings.toml'] )
 
 __all__ = ['settings']
